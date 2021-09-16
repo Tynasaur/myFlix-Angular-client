8cd3c3f6a9aefd6d545ae05a2897c96cb62e0960
Tynasaur <70995431+Tynasaur@users.noreply.github.com>
2021-09-16 10:51:41 -0500

    styled movie-card

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { FetchApiDataService } from '../fetch-api-data.service';
 import { DirectorViewComponent } from '../director-view/director-view.component';
 import { GenreViewComponent } from '../genre-view/genre-view.component';
-import { DetailsDialogComponent } from '../details-dialog/details-dialog.component';
+import { MovieDetailsComponent } from '../movie-details/details-dialog.component';
 
 import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -16,12 +16,17 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class MovieCardComponent {
   movies: any[] = [];
-  constructor(public fetchApiData: FetchApiDataService,
+  faves: any[] = [];
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
     public dialog: MatDialog,
-    public snackBar: MatSnackBar,) {}
+    public snackBar: MatSnackBar
+  ) {}
 
   ngOnInit(): void {
     this.getMovies();
+    this.getUsersFavs();
   }
 
   getMovies(): void {
@@ -50,16 +55,59 @@ export class MovieCardComponent {
       panelClass: 'director-dialog',
     });
   }
-  showDetailsDialog(
+  showMovieDetails(
     title: string,
     imagePath: string,
     description: string,
     director: string,
     genre: string
   ): void {
-    this.dialog.open(DetailsDialogComponent, {
+    this.dialog.open(MovieDetailsComponent, {
       data: { title, imagePath, description, director, genre },
       panelClass: 'details-dialog',
     });
+  }
+
+  addToFavoriteMoviesList(id: string, Title: string): void {
+    this.fetchApiData.addToFavoriteMovies(id).subscribe((res: any) => {
+      this.snackBar.open(`${Title} has been added to favorties`, 'OK', {
+        duration: 3000,
+      });
+      return this.getUsersFavs();
+    });
+  }
+
+  /**
+   * Removed movie from users favorites list
+   */
+  removeFromFavorites(id: string, Title: string): void {
+    this.fetchApiData.removeFavoriteMovie(id).subscribe((res: any) => {
+      this.snackBar.open(`${Title} has been removed from favorties`, 'OK', {
+        duration: 3000,
+      });
+      return this.getUsersFavs();
+    });
+  }
+
+  /**
+   * Returns a list of the users favorites movie._id's
+   */
+  getUsersFavs(): void {
+    const user = localStorage.getItem('username');
+    this.fetchApiData.getUser(user).subscribe((resp: any) => {
+      this.faves = resp.Favorites;
+      //console.log(this.faves);
+      return this.faves;
+    });
+  }
+  /**
+   * Compares movie id's with getUsersFavs returned list to set the Favorites icon to add/remove correctly
+   */
+  setFaveStatus(id: any): any {
+    if (this.faves.includes(id)) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
